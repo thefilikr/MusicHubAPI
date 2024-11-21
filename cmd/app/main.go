bdcba21d1bc50f24000327cd620e34cfeee8627f
thefilikr <filikrepic@gmal.com>
2024-11-21 18:00:43 +0300

    load config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Env            string    `yaml:"env"`
+	App            ConfigApp `yaml:"app"`
+	DB             ConfigDB  `yaml:"db"`
+	MigrationsPath string    `yaml:"migrations_path"`
+}
+
+type ConfigApp struct {
+	PortApp uint `yaml:"port_app"`
+}
+
+type ConfigDB struct {
+	PortDB   uint   `yaml:"port_db"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
+func LoadConfig(filePath string) (*Config, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось прочитать файл: %w", err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось декодировать YAML: %w", err)
+	}
+
+	return &config, nil
+}
+
 func main() {
-	// TODO конфигурация
+
+	config, _ := LoadConfig("./../../configs/config.yaml")
+
+	fmt.Println(config)
 
 	// TODO логер
 
